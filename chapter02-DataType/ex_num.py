b5d3e6fceaf33070241140372e598422e447e7a1
LEEGYUHEE <leegyuhee@LEEGYUHEEui-MacBookPro.local>
2020-07-31 23:00:10 +0900

    폴더명 변경, 문자열 예제 추가, 숫자 예제

---
+++ chapter02-DataType/ex_num.py
@@ -0,0 +1,34 @@
+# 숫자의 종류
+print(type(273)) # int 정수
+print(type(52.273)) # float 실수
+print(type(0)) # int 정수
+print(type(0.0)) # float 실수
+print()
+
+# 숫자 연산자
+# 사칙 연산자 : +,-,*,/
+print("5 + 7 = ", 5+7)
+print("5 - 7 = ", 5-7)
+print("5 * 7 = ", 5*7)
+print("5 / 7 = ", 5/7)
+print()
+# 정수 나누기 연산자 : //
+print("3 / 2 = ", 3/2) # 1.5
+print("3 // 2 = ", 3//2) # 1
+print()
+# 나머지 연산자 : %
+print("5 % 2 = ", 5%2)
+print()
+# 제곱 연산자 : **
+print("2 ** 1", 2**1)
+print("2 ** 2", 2**2)
+print("2 ** 3", 2**3)
+print("2 ** 4", 2**4)
+print()
+
+# 연산자의 우선순위
+print(2 + 2 - 2 * 2 / 2 * 2)
+print(2 - 2 + 2 / 2 * 2 + 2)
+print((5 + 3) * 2)
+print(5 + (3 * 2))
+print()
