753c258e58eeedf2463e69c367cf4dcbc827647e
LEEGYUHEE <leegyuhee@LEEGYUHEEui-MacBookPro.local>
2020-08-04 22:46:57 +0900

    문자열 함수

--- chapter02-DataType/ex_string.py
+++ chapter02-DataType/ex_string.py
@@ -92,3 +92,44 @@ print()
 
 # 문자열의 길이 구하기
 print(len("안녕하세요"))
+
+# 문자열 양옆의 공백 없애기 : strip()
+input_a = """ 
+    안녕하세요
+문자열 함수를 알아봅니다
+"""
+print(input_a)
+print(input_a.strip())
+print()
+
+# 문자열의 구성 파악하기 : isOO()
+# isalnum() : 문자열이 알파벳 또는 숫자로만 구성이 되어 있는지 확인
+# isalpha() : 문자열이 알파벳으로만 구성이 되어 있는지 확인
+# isidentifier() : 문자열이 식별자로 사용할 수 있는 것인지 확인
+# isdecimal() : 문자열이 정수 형태인지 확인
+# isdigit() : 문자열이 숫자로 인식될 수 있는 것인지 확인
+# isspace() : 문자열이 공백으로만 구성되어 있는지 확인
+# islower() : 문자열이 소문자로만 구성되어 있는지 확인
+# isupper() : 문자열이 대문자로만 구성되어 있는지 확인
+print("TrainA10".isalnum())
+print("10".isdigit())
+print("ABCD".islower())
+print()
+
+# 문자열 찾기
+# find() : 왼쪽부터 찾아서 처음 등장하는 위치를 찾음
+# rfind() : 오른쪽부터 찾아서 처음 등장하는 위치를 찾음
+output_a = "안녕하세요".find("안녕")
+print(output_a)
+output_b = "안녕안녕하세요".rfind("안녕")
+print(output_b)
+print()
+
+# 문자열과 in 연산자
+print("안녕" in "안녕하세요")
+print("잘자" in "안녕하세요")
+print()
+
+# 문자열 자르기 : split()
+a = "10 20 30 40 50".split(" ")
+print(a)
