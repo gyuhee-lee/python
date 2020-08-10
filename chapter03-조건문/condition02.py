8d1df49fd3e337f4c3cbd4c68d7bc4afffe3f65c
LEEGYUHEE <leegyuhee@LEEGYUHEEui-MacBookPro.local>
2020-08-10 23:32:29 +0900

    elif, 구현하지 않은 부분 강제 오류 발생

---
+++ chapter03-조건문/condition02.py
@@ -0,0 +1,11 @@
+# 입력을 받습니다
+number = input("정수 입력 > ")
+number = int(number)
+
+# 조건문 사용
+if number % 2 == 0 :
+    # 조건이 참일때 실행
+    print("짝수입니다.")
+else :
+    # 조건이 거짓일때 실행
+    print("홀수입니다.")
---
+++ chapter03-조건문/pass_keyword01.py
@@ -0,0 +1,12 @@
+number = input("정수 입력 > ")
+number = int(number)
+
+# 조건문 사용
+if number > 0 :
+    # 양수일 때 : 아직 미구현 상태입니다.
+    raise NotImplementedError
+else : 
+    # 음수일 때 : 아직 미구현 상태입니다.
+    raise NotImplementedError
+
+# raise NotImplementedError 사용하여 아직 구현하지 않은 부분이라고 오류를 강제로 발생시킬 수도 있음
